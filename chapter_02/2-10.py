df2996eedc3a052c82840fcfb1e7c4e72f79fc73
lethons <lethons@163.com>
2018-04-01 19:43:57 +0800

    uploads files

---
+++ chapter_02/2-10.py
@@ -0,0 +1,9 @@
+"""
+ * @Author: Lethons 
+ * @Date: 2018-04-01 19:38:48 
+ * @Last Modified by:   lethons 
+ * @Last Modified time: 2018-04-01 19:38:48 
+"""
+
+favorite_number = 7
+print('My favorite number is ' + str(favorite_number) + '.')
